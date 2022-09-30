015ad808bc0727bdda11ca64f59d52bb9a795a83
Christian <christian.auman@outlook.com>
2022-09-29 20:53:53 -0400

    Animations

--- mainpage/pages/index.js
+++ mainpage/pages/index.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import Image from 'next/image';
+import Image from 'next/image'; 
+import {motion} from 'framer-motion'; 
+import { forwardRef, useState } from 'react';
 
 // assets
 import background from '/public/page-backdrop.jpg';
@@ -16,6 +18,14 @@ const Styles = styled.div`
     width: 100vw;
     height: 100vh;
     background-size: cover;
+  } 
+  & .green-ring {  
+    width: 100%;
+    overflow: visible;
+    stroke: #80ed99;
+    stroke-width: 6;
+    stroke-linejoin: round;
+    stroke-linecap: round; 
   }
 `;
 
@@ -47,18 +57,108 @@ const NameText = styled.h1`
  }
 `;
 
-const Home = () => {
+const Home = () => {  
+  const letterVariants = {
+    initial: i => ({
+      x: 90*i,   
+      opacity: 0,
+    }),
+    visible: {
+      x: 0, 
+      opacity: 1,
+      transition: { 
+        type: 'spring', 
+        duration: 0.7,
+        stiffness: 90,
+        damping: 20,
+        mass: 1,
+        bounce: 0.25
+      }
+    },
+  } 
+  const titleVariants = {
+    initial: {
+      // y: -100,
+      opacity: 0,
+      scale: 1.2
+    },
+    visible: {
+      y: 0, 
+      opacity: 1,
+      scale: 1,
+      transition: { 
+        type: 'spring', 
+        duration: 0.7,
+        stiffness: 70,
+        damping: 30,
+        mass: 1,
+        bounce: 0
+      }
+    }, 
+  }
+  const buttonVariants = {
+    initial: { 
+      opacity: 0,
+      scale: 0.8
+    },
+    visible: {  
+      opacity: 1,
+      scale: 1,
+      transition: { 
+        type: 'spring', 
+        duration: 0.7,
+        stiffness: 100,
+        damping: 30,
+        mass: 0.2,
+        bounce: 0
+      }
+    }, 
+  }
+  const greenRingVariants = {
+    initial: {
+      pathLength: 0,  
+      opacity: 0,
+      rotate: 300,
+      transition: { 
+        type: 'spring',  
+        stiffness: 70,
+        damping: 60,
+        mass: 1,
+        bounce: 0
+      }
+    },
+    visible: {
+      pathLength: 1,
+      opacity: [0,1,1,1],
+      rotate: -90,
+      transition: { 
+        type: 'spring',  
+        stiffness: 40,
+        damping: 10,
+        mass: 1,
+        bounce: 0
+      }
+    }
+  } 
+  const [playRingAnim, setPlayRingAnim] = useState(false);
   return (
     <Styles> 
-      <NameText>Christian Auman</NameText>
+      <motion.div variants={titleVariants} initial='initial' whileInView='visible' viewport={{once: true}}><NameText>Christian Auman</NameText></motion.div>
       <div className="w-full flex justify-center gap-x-36 items-center">
-        <Image src={C.src} width={428} height={428} />
-        <StartButton>
-          <svg width="99" height="99" viewBox="0 0 99 99" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M49.4999 68.3333L68.3333 49.5M68.3333 49.5L49.4999 30.6667M68.3333 49.5L30.6666 49.5M96.5833 49.5C96.5833 75.5034 75.5034 96.5833 49.5 96.5833C23.4966 96.5833 2.41663 75.5034 2.41663 49.5C2.41663 23.4966 23.4966 2.41666 49.5 2.41666C75.5034 2.41666 96.5833 23.4966 96.5833 49.5Z" stroke="#FCFDFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg> 
-        </StartButton>
-        <Image src={S.src} width={428} height={428} />
+        <motion.div variants={letterVariants} custom={-1} initial='initial' whileInView='visible' viewport={{once: true}}><Image src={C.src} width={428} height={428} /></motion.div>
+        <motion.div onHoverStart={() => {setPlayRingAnim(true)}} onHoverEnd={() => {setPlayRingAnim(false)}} variants={buttonVariants} initial='initial' whileInView='visible' viewport={{once: true}}>
+          <StartButton >
+            <svg width="99" height="99" viewBox="0 0 99 99" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M49.4999 68.3333L68.3333 49.5M68.3333 49.5L49.4999 30.6667M68.3333 49.5L30.6666 49.5M96.5833 49.5C96.5833 75.5034 75.5034 96.5833 49.5 96.5833C23.4966 96.5833 2.41663 75.5034 2.41663 49.5C2.41663 23.4966 23.4966 2.41666 49.5 2.41666C75.5034 2.41666 96.5833 23.4966 96.5833 49.5Z" stroke="#FCFDFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg> 
+
+            <motion.svg className='absolute' width="103" height="103" viewBox="0 0 103 103" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <motion.path className='green-ring' variants={greenRingVariants} initial='initial' animate={playRingAnim?'visible':'initial'} d="M51.5 100.25C78.4239 100.25 100.25 78.4239 100.25 51.5C100.25 24.5761 78.4239 2.75 51.5 2.75C24.5761 2.75 2.75 24.5761 2.75 51.5C2.75 78.4239 24.5761 100.25 51.5 100.25Z" stroke="#67F29F" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+            </motion.svg>
+
+          </StartButton>
+        </motion.div>
+        <motion.div variants={letterVariants} custom={1} initial='initial' whileInView='visible' viewport={{once: true}}><Image src={S.src} width={428} height={428} /></motion.div>
       </div>
     </Styles>
   )
