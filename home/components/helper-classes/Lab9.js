e502140def542f48312c08667a0af2f300480c6d
Christian <christian.auman@outlook.com>
2022-11-03 02:38:26 -0400

    Update

--- home/components/helper-classes/Lab9.js
+++ home/components/helper-classes/Lab9.js
@@ -2,7 +2,7 @@
 class Lab9 { 
     static GetLinks = () => {
         return [
-			{href: '/lab-9/google-maps', title: 'Google Maps', link: true}
+			{href: '/lab-9/google-api', title: 'Google API', link: true}
         ]
     }  
 }
